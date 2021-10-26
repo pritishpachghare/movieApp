ceb93114930bc78d3713f4384bf8e6cdb913fa5f
Pritish Pachghare <pritishpachghare@gmail.com>
2021-10-25 23:58:33 -1200

    first commit

---
+++ utils.js
@@ -0,0 +1,27 @@
+
+const debounce = (func, delay = 1000) => {
+    let timeOutId;
+    return (...args) => {
+        if (timeOutId) {
+            clearTimeout(timeOutId)
+        }
+        timeOutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
